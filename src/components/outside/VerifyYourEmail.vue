103445c5c122d581f461cfa1860d5f6f32676366
kajetan <katek1094@interia.pl>
2021-04-13 09:01:25 +0200

    email timeout

--- src/components/outside/VerifyYourEmail.vue
+++ src/components/outside/VerifyYourEmail.vue
@@ -7,7 +7,7 @@
           rejestracji. </p>
         <p>Jeżeli nie widzisz wiadomości w skrzynce odbiorczej, należy sprawdzić foldery "Spam", "Oferty" itp. </p>
         <p>Na niektóre skrzynki mailowe wiadomość może przyjść nawet po 10-15 minutach. </p>
-        <p>Uwaga! Konto należy aktywować w ciągu 30 minut od jego utworzenia.</p>
+        <p>Uwaga! Link aktywacyjny jest ważny przez 24 godziny od utworzenia konta, potem nalezy zarejestrować się ponownie. </p>
       </div>
     </div>
   </OutsideBaseApp>

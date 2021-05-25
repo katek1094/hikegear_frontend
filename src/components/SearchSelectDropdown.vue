08e4e6c3c4de7b11b93b7f930e55e812abd4443e
kajetan <katek1094@interia.pl>
2021-05-25 18:55:07 +0200

    created dropdown select search component for brand selection

---
+++ src/components/SearchSelectDropdown.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="wrapper">
+    <input class="search" type="text" v-model="query" :placeholder="placeholder" @click="activate"
+           @blur="deactivate" @input="search">
+    <div v-if="activated" class="hints">
+      <span class="hint" v-for="res in results" :key="res.id" @mousedown="select($event, res)">{{ res.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  name: "SearchSelectDropdown",
+  props: {values: Array, placeholder: String},
+  emits: {selected: ({id}) => (typeof id === 'number')},
+  setup(props, context) {
+    const query = ref('')
+    const activated = ref(false)
+    const results = ref([])
+
+    const activate = () => {
+      activated.value = true
+      search()
+    }
+    const deactivate = () => activated.value = false
+
+    const search = () => {
+      let res = []
+      for (let i = 0; i < props.values.length; i++) {
+        const s = props.values[i].name.toLowerCase().search(query.value.toLowerCase())
+        if (s >= 0) {
+          res.push(props.values[i])
+          res[res.length - 1].sindex = s
+        }
+      }
+      res.sort((a, b) => {
+        return a.sindex - b.sindex
+      })
+      results.value = res
+    }
+
+    const select = (e, x) => {
+      if (e.button === 0) {
+        query.value = x.name
+        search()
+        context.emit('selected', {id: x.id})
+      }
+    }
+
+    const reset = () => {
+      query.value = ''
+      activated.value = false
+    }
+
+    return {query, activated, results, activate, deactivate, search, select, reset}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  border-radius: 4px;
+  overflow: hidden;
+  margin: 4px;
+  display: inline-block;
+}
+
+.search {
+  width: 100%;
+  border: none;
+  outline: none;
+  box-shadow: none;
+}
+
+.search, .hint {
+  padding: 5px 5px 5px 10px;
+  box-sizing: border-box;
+  font-size: 1rem;
+}
+
+.hints {
+  position: absolute;
+  max-height: 300px;
+  width: inherit;
+  overflow-y: auto;
+  box-sizing: content-box;
+}
+
+.hint {
+  display: block;
+  background-color: white;
+
+  &:hover {
+    background-color: cornflowerblue;
+    color: white;
+    cursor: pointer;
+  }
+}
+
+</style>
--- src/components/inside/reviews/Reviews.vue
+++ src/components/inside/reviews/Reviews.vue
@@ -2,40 +2,46 @@
   <InsideBaseApp>
     <div class="reviews hg-flx_col_ctr">
       <div>
-        <input class="hg-input" type="text" placeholder="wyszukaj produkt" v-model="search_input_value">
-        <p><b>filtry</b></p>
-        <div>
-          <label for="select_category">wybierz kategorię</label>
-          <select id="select_category" class="hg-select" v-model="selected_category_id">
-            <option disabled hidden selected value="">wybierz kategorię</option>
-            <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
-          </select>
-          <button v-show="selected_category_id" class="reset_button" @click="resetCategoryFilter">
-            <font-awesome-icon class="fa-lg" icon="times"/>
-          </button>
+        <input class="hg-input search_product" type="text" placeholder="wyszukaj produkt"
+               v-model="search_product_query">
+        <div class="filters">
+          <span><b>filtry</b></span>
+          <div class="field">
+            <label for="select_category">wybierz kategorię</label>
+            <select id="select_category" class="hg-select" v-model="selected_category_id">
+              <option disabled hidden selected value="">wybierz kategorię</option>
+              <option v-for="category in categories" :key="category.id" :value="category.id">
+                {{ category.name }}
+              </option>
+            </select>
+            <button v-show="selected_category_id" class="reset_button" @click="resetCategoryFilter">
+              <font-awesome-icon class="fa-lg" icon="times"/>
+            </button>
+          </div>
+          <div class="field" v-if="selected_category_id != null">
+            <label for="select_subcategory">wybierz podkategorię</label>
+            <select id="select_subcategory" class="hg-select" v-model="selected_subcategory_id">
+              <option disabled hidden selected value="">wybierz podkategorię</option>
+              <option v-for="subcategory in subcategories"
+                      :key="subcategory.id"
+                      :value="subcategory.id">{{ subcategory.name }}
+              </option>
+            </select>
+            <button v-show="selected_subcategory_id" class="reset_button" @click="resetSubcategoryFilter">
+              <font-awesome-icon class="fa-lg" icon="times"/>
+            </button>
+          </div>
+          <div class="field">
+            <label>wybierz producenta</label>
+            <SearchSelectDropdown :values="brands" placeholder="wybierz producenta" @selected="setBrandId"
+                                  ref="brand_select" class="brand_select"/>
+            <button v-show="selected_brand_id" class="reset_button" @click="resetBrandFilter">
+              <font-awesome-icon class="fa-lg" icon="times"/>
+            </button>
+          </div>
         </div>
-        <div v-if="selected_category_id != null">
-          <label for="select_subcategory">wybierz podkategorię</label>
-          <select id="select_subcategory" class="hg-select" v-model="selected_subcategory_id">
-            <option disabled hidden selected value="">wybierz podkategorię</option>
-            <option v-for="subcategory in subcategories"
-                    :key="subcategory.id"
-                    :value="subcategory.id">{{ subcategory.name }}
-            </option>
-          </select>
-        </div>
-        <div>
-          <label for="select_brand">wybierz producenta</label>
-          <select id="select_brand" class="hg-select" v-model="selected_brand_id">
-            <option disabled hidden selected value="">wybierz producenta</option>
-            <option v-for="brand in brands" :key="brand.id" :value="brand.id">{{ brand.name }}</option>
-          </select>
-          <button v-show="selected_brand_id" class="reset_button" @click="resetBrandFilter">
-            <font-awesome-icon class="fa-lg" icon="times"/>
-          </button>
-        </div>
-        <button class="hg-button">szukaj</button>
 
+        <button class="hg-button" @click="submitSearch">szukaj</button>
 
       </div>
     </div>
@@ -46,10 +52,12 @@
 import InsideBaseApp from "../InsideBaseApp";
 import {ref, watch, computed} from 'vue'
 import {useStore} from 'vuex'
+import {apiFetch} from "@/functions";
+import SearchSelectDropdown from "../../SearchSelectDropdown";
 
 export default {
   name: "Reviews",
-  components: {InsideBaseApp},
+  components: {SearchSelectDropdown, InsideBaseApp},
   setup() {
     const store = useStore()
     const categories = computed(() => store.getters['reviews/categories'])
@@ -58,7 +66,8 @@ export default {
     const selected_category_id = ref(null)
     const selected_subcategory_id = ref(null)
     const selected_brand_id = ref(null)
-    const search_input_value = ref('')
+    const search_product_query = ref('')
+    const brand_select = ref('null') //template ref
 
     const subcategories = computed(() => {
       return categories.value.find((el) => el.id === selected_category_id.value).subcategories
@@ -67,19 +76,64 @@ export default {
     watch(selected_category_id, () => selected_subcategory_id.value = null)
 
     const resetCategoryFilter = () => selected_category_id.value = null
-    const resetBrandFilter = () => selected_brand_id.value = null
+    const resetSubcategoryFilter = () => selected_subcategory_id.value = null
+    const resetBrandFilter = () => {
+      selected_brand_id.value = null
+      brand_select.value.reset()
+    }
+
+    const setBrandId = (payload) => selected_brand_id.value = payload.id
+
+    const submitSearch = () => {
+      apiFetch('search_for_product?' + new URLSearchParams({
+        query: search_product_query.value
+      }), {
+        method: 'GET'
+      })
+          .then(response => {
+            if (response.ok) {
+              response.json().then(data => {
+                console.log(data)
+              })
+            } else console.log(response)
+          })
+    }
 
     return {
-      categories, subcategories, search_input_value, brands, selected_category_id, selected_subcategory_id,
-      selected_brand_id, resetCategoryFilter, resetBrandFilter
+      categories,
+      subcategories,
+      search_product_query,
+      brand_select,
+      brands,
+      selected_category_id,
+      selected_subcategory_id,
+      selected_brand_id,
+      resetCategoryFilter,
+      resetSubcategoryFilter,
+      resetBrandFilter,
+      submitSearch,
+      setBrandId
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.search_product {
+  width: 30rem;
+}
+
 .hg-select {
   margin: 4px;
+}
+
+.field {
+  display: flex;
+  align-items: center;
+}
+
+.brand_select {
+  width: 200px;
 }
 
 .reset_button {
